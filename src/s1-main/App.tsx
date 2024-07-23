bda08f0fceec9a7e26ba4e41b010f25c06cb7281
vetal1136 <komrad188@yandex.ru>
2024-07-23 14:37:11 +0300

    3 sprint 4 week 4 homework

--- src/s1-main/App.tsx
+++ src/s1-main/App.tsx
@@ -7,6 +7,7 @@ import HW4 from "../s2-homeworks/hw04/HW4";
 import HW5 from "../s2-homeworks/hw05/HW5";
 import HW6 from "../s2-homeworks/hw06/HW6";
 import HW7 from "../s2-homeworks/hw07/HW7";
+import HW8 from "../s2-homeworks/hw08/HW8";
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
             <HW5/>
             <HW6/>
             <HW7/>
+            <HW8/>
         </div>
     )
 }
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,29 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if (action.payload === 'up') {
+                const newState = [...state]
+                return newState.sort((a, b) => {
+                    if (a.name > b.name) return 1
+                    else return -1
+                })
+            } else if (action.payload === 'down') {
+                const newState = [...state]
+                return newState.sort((a, b) => {
+                    if (a.name < b.name) return 1
+                    else return -1
+                })
+            } else return [...state]
+            // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(u => u.age >= action.payload)
+            // need to fix
         }
         default:
-            return state
+            return [...state]
     }
 }
